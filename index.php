7cf20cf075919afde8a526d906067cf61f83020e
Mostofa Kamal <80709877+Mostofa2097@users.noreply.github.com>
2021-03-17 23:34:02 +0600

    added html CSS file for CV

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+   <meta charset="UTF-8">
+   <meta name"viewport" content="width=divice-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie-edge">
+   <title>
+    Mostofa Kamal's CV  
+   </title>
+   <link  rel="styleseet" 
+</head>
+<body>
+  <div>
+    <header>
+       gdfjg
+    </header>
+    <main>
+    
+    </main>
+    <footer>
+    
+    </footer>
+   <div>
+</body>
+</html>
